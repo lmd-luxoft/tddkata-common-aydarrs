d5ff326a3cac8e7cdff41f70a75886b0a928f698
Aydar Safiullin <adsaf16@yandex.ru>
2021-04-13 13:29:40 +0300

    task 2

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -5,6 +5,21 @@
  */
 public class Calc {
     public int sum(String expression) {
-        throw new UnsupportedOperationException();
+        if ("".equals(expression)) {
+            return 0;
+        }
+
+        int result = 0;
+        try {
+            String[] numbers = expression.split(",");
+            for (String number : numbers) {
+                number.trim();
+                result += Integer.parseInt(number);
+            }
+        } catch (Exception e) {
+                return -1;
+        }
+
+        return result;
     }
 }
