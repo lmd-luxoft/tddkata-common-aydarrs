fd30a9d1ed5f188572e3e5dbbfcdca2e8ae7ae89
Aydar Safiullin <adsaf16@yandex.ru>
2021-04-13 13:02:20 +0300

    task 1

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * CalcTest.
@@ -6,8 +9,31 @@ import org.junit.jupiter.api.Test;
  * @author Aydar_Safiullin
  */
 public class CalcTest {
-    @Test
-    public void testCalcMethod() {
+    Calc calc;
 
+    @BeforeEach
+    public void init() {
+        calc = new Calc();
+    }
+
+    @Test
+    public void testEmptyStringSum() {
+        int actual = calc.sum("");
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSingleNumberStringSum() {
+        int actual = calc.sum("1");
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTwoNumbersSum() {
+        int actual = calc.sum("1,2");
+        int expected = 3;
+        assertEquals(expected, actual);
     }
 }
