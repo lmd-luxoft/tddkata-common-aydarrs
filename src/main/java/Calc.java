a3fc09a1b377a661c9a2f60dcda9088c83566421
Aydar Safiullin <adsaf16@yandex.ru>
2021-04-14 10:44:09 +0300

    task 5

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -11,15 +11,27 @@ public class Calc {
 
         int result = 0;
         try {
-            String[] numbers = expression.split(",");
-            for (String number : numbers) {
-                number.trim();
-                result += Integer.parseInt(number);
+            if (expression.contains(",\n") || expression.contains("\n,")) {
+                return -1;
             }
+            String[] numbers = expression.split(",");
+            result = parse(result, numbers);
         } catch (Exception e) {
                 return -1;
         }
 
+        return result;
+    }
+
+    private int parse(int result, String[] numbers) {
+        for (String number : numbers) {
+            if (number.contains("\n")) {
+                String[] entryNumbers = number.split("\n");
+                result += parse(result, entryNumbers);
+            } else {
+                result += Integer.parseInt(number.trim());
+            }
+        }
         return result;
     }
 }
